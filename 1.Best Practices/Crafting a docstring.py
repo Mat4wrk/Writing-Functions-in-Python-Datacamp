9b1af211915d277bd910defe4419a15b37cf408b
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 13:59:49 +0700

    Update Crafting a docstring.py

--- 1.Best Practices/Crafting a docstring.py
+++ 1.Best Practices/Crafting a docstring.py
@@ -7,8 +7,8 @@ def count_letter(content, letter):
   return len([char for char in content if char == letter])
   
   
-  """Now add the arguments section, using the Google style for docstrings. Use str to indicate a string."""
-  def count_letter(content, letter):
+"""Now add the arguments section, using the Google style for docstrings. Use str to indicate a string."""
+def count_letter(content, letter):
   """Count the number of times `letter` appears in `content`.
 
   # Add a Google-style arguments section
@@ -21,7 +21,7 @@ def count_letter(content, letter):
   return len([char for char in content if char == letter])
   
   
-  """Add a returns section that informs the user the return value is an int."""
+"""Add a returns section that informs the user the return value is an int."""
 def count_letter(content, letter):
   """Count the number of times `letter` appears in `content`.
 
@@ -39,6 +39,16 @@ def count_letter(content, letter):
 
 
 """Finally, add some information about the ValueError that gets raised when the arguments aren't correct."""
+def count_letter(content, letter):
+  """Count the number of times `letter` appears in `content`.
+
+  Args:
+    content (str): The string to search.
+    letter (str): The letter to search for.
+
+  Returns:
+    int
+
   # Add a section detailing what errors might be raised
   Raises:
     ValueError: If `letter` is not a one-character string.
