55e9be5f0d5cb44e4b93d622c506790fd511f2ef
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 14:19:01 +0700

    Create Defining a decorator.py

---
+++ 3.Decorators/Defining a decorator.py
@@ -0,0 +1,14 @@
+def print_before_and_after(func):
+  def wrapper(*args):
+    print('Before {}'.format(func.__name__))
+    # Call the function being decorated with *args
+    func(*args)
+    print('After {}'.format(func.__name__))
+  # Return the nested function
+  return wrapper
+
+@print_before_and_after
+def multiply(a, b):
+  print(a * b)
+
+multiply(5, 10)
