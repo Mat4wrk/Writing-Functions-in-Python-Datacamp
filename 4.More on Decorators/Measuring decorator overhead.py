c6f4980eb0197dcbed9a0683d03ecb533d926ac1
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 14:25:20 +0700

    Create Measuring decorator overhead.py

---
+++ 4.More on Decorators/Measuring decorator overhead.py
@@ -0,0 +1,18 @@
+@check_everything
+def duplicate(my_list):
+  """Return a new list that repeats the input twice"""
+  return my_list + my_list
+
+t_start = time.time()
+duplicated_list = duplicate(list(range(50)))
+t_end = time.time()
+decorated_time = t_end - t_start
+
+t_start = time.time()
+# Call the original function instead of the decorated one
+duplicated_list = duplicate.__wrapped__(list(range(50)))
+t_end = time.time()
+undecorated_time = t_end - t_start
+
+print('Decorated time: {:.5f}s'.format(decorated_time))
+print('Undecorated time: {:.5f}s'.format(undecorated_time))
