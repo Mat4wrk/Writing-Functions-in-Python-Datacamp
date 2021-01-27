3f19f5b09d081004cff87686d7a39f6118560c51
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 14:15:06 +0700

    Create Checking for closure.py

---
+++ 3.Decorators/Checking for closure.py
@@ -0,0 +1,45 @@
+"""Use an attribute of the my_func() function to show that it has a closure that is not None."""
+def return_a_func(arg1, arg2):
+  def new_func():
+    print('arg1 was {}'.format(arg1))
+    print('arg2 was {}'.format(arg2))
+  return new_func
+    
+my_func = return_a_func(2, 17)
+
+# Show that my_func()'s closure is not None
+print(my_func.__closure__ is not None)
+
+
+"""Show that there are two variables in the closure."""
+def return_a_func(arg1, arg2):
+  def new_func():
+    print('arg1 was {}'.format(arg1))
+    print('arg2 was {}'.format(arg2))
+  return new_func
+    
+my_func = return_a_func(2, 17)
+
+print(my_func.__closure__ is not None)
+
+# Show that there are two variables in the closure
+print(len(my_func.__closure__) == 2)
+
+
+"""Get the values of the variables in the closure so you can show that they are equal to [2, 17], the arguments passed to return_a_func()."""
+def return_a_func(arg1, arg2):
+  def new_func():
+    print('arg1 was {}'.format(arg1))
+    print('arg2 was {}'.format(arg2))
+  return new_func
+    
+my_func = return_a_func(2, 17)
+
+print(my_func.__closure__ is not None)
+print(len(my_func.__closure__) == 2)
+
+# Get the values of the variables in the closure
+closure_values = [
+  my_func.__closure__[i].cell_contents for i in range(2)
+]
+print(closure_values == [2, 17])
