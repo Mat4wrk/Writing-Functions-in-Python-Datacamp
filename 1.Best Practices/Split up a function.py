5c8eb8f420f33ff741c6cad98839686c8d21752c
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 13:53:57 +0700

    Create Split up a function.py

---
+++ 1.Best Practices/Split up a function.py
@@ -0,0 +1,33 @@
+"""Write the mean() function."""
+def mean(values):
+  """Get the mean of a list of values
+
+  Args:
+    values (iterable of float): A list of numbers
+
+  Returns:
+    float
+  """
+  # Write the mean() function
+  mean = sum(values) / len(values)
+  return mean
+  
+  
+"""Write the median() function."""
+def median(values):
+  """Get the median of a list of values
+
+  Args:
+    values (iterable of float): A list of numbers
+
+  Returns:
+    float
+  """
+  # Write the median() function
+  midpoint = int(len(values) / 2)
+  if len(values) % 2 == 0:
+    median = (values[midpoint - 1] + values[midpoint]) / 2
+  else:
+    median = values[midpoint]
+
+  return median
