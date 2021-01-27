3d78710c37236c7d407f979d49b6a4754f46bce5
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 14:33:51 +0700

    Create Check the return type.py

---
+++ 4.More on Decorators/Check the return type.py
@@ -0,0 +1,39 @@
+"""Start by completing the returns_dict() decorator so that it raises an AssertionError if the return type of the decorated function is not a dictionary."""
+def returns_dict(func):
+  # Complete the returns_dict() decorator
+  def wrapper(func):
+    result = AssertionError
+    assert(type(result) == dict)
+    return result
+  return wrapper
+  
+@returns_dict
+def foo(value):
+  return value
+
+try:
+  print(foo([1,2,3]))
+except AssertionError:
+  print('foo() did not return a dict!')
+  
+
+"""Now complete the returns() decorator, which takes the expected return type as an argument."""
+def returns(return_type):
+  # Write a decorator that raises an AssertionError if the
+  # decorated function returns a value that is not return_type
+  def decorator(func):
+    def wrapper(*args, **kwargs):
+      result = func(*args, **kwargs)
+      assert(type(result) == return_type)
+      return result
+    return wrapper
+  return decorator
+  
+@returns(dict)
+def foo(value):
+  return value
+
+try:
+  print(foo([1,2,3]))
+except AssertionError:
+  print('foo() did not return a dict!')
