bcb7871c944205d0ff2e63acb117b4cef1dcb7a4
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 14:16:25 +0700

    Create Closures keep your values safe.py

---
+++ 3.Decorators/Closures keep your values safe.py
@@ -0,0 +1,48 @@
+"""how that you still get the original message even if you redefine my_special_function() to only print "hello"."""
+def my_special_function():
+  print('You are running my_special_function()')
+  
+def get_new_func(func):
+  def call_func():
+    func()
+  return call_func
+
+new_func = get_new_func(my_special_function)
+
+# Redefine my_special_function() to just print "hello"
+def my_special_function():
+  print("hello")
+
+new_func()
+
+
+"""Show that even if you delete my_special_function(), you can still call new_func() without any problems."""
+def my_special_function():
+  print('You are running my_special_function()')
+  
+def get_new_func(func):
+  def call_func():
+    func()
+  return call_func
+
+new_func = get_new_func(my_special_function)
+
+# Delete my_special_function()
+del(my_special_function)
+
+new_func()
+
+
+"""Show that you still get the original message even if you overwrite my_special_function() with the new function."""
+def my_special_function():
+  print('You are running my_special_function()')
+  
+def get_new_func(func):
+  def call_func():
+    func()
+  return call_func
+
+# Overwrite `my_special_function` with the new function
+my_special_function = get_new_func(my_special_function)
+
+my_special_function()
