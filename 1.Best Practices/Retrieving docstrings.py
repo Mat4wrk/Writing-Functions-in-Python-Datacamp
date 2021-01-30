8ad2c7e04e384d761f74904f92a19b8de21b802f
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 15:59:55 +0700

    Update Retrieving docstrings.py

--- 1.Best Practices/Retrieving docstrings.py
+++ 1.Best Practices/Retrieving docstrings.py
@@ -18,15 +18,15 @@ print('{}\n{}\n{}'.format(border, docstring, border))
 
 # Use the inspect module again to get the docstring for any function being passed to the build_tooltip() function.
 def build_tooltip(function):
-  """Create a tooltip for any function that shows the 
-  function's docstring.
+#   Create a tooltip for any function that shows the 
+#   function's docstring.
   
-  Args:
-    function (callable): The function we want a tooltip for.
+#   Args:
+#     function (callable): The function we want a tooltip for.
     
-  Returns:
-    str
-  """
+#   Returns:
+#     str
+  
   # Use 'inspect' to get the docstring
   docstring = inspect.getdoc(function)
   border = '#' * 28
