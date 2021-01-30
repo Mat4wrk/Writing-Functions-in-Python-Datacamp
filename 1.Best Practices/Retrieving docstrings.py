9822f60cc8eaf695313ce3a01c8b20bc5dcd2816
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 15:53:23 +0700

    Update Retrieving docstrings.py

--- 1.Best Practices/Retrieving docstrings.py
+++ 1.Best Practices/Retrieving docstrings.py
@@ -1,4 +1,4 @@
-"""Begin by getting the docstring for the function count_letter(). Use an attribute of the count_letter() function."""
+# Begin by getting the docstring for the function count_letter(). Use an attribute of the count_letter() function.
 # Get the docstring with an attribute of count_letter()
 docstring = count_letter.__doc__
 
@@ -6,7 +6,7 @@ border = '#' * 28
 print('{}\n{}\n{}'.format(border, docstring, border))
 
 
-"""Now use a function from the inspect module to get a better-formatted version of count_letter()'s docstring."""
+# Now use a function from the inspect module to get a better-formatted version of count_letter()'s docstring.
 import inspect
 
 # Get the docstring with a function from the inspect module
@@ -16,7 +16,7 @@ border = '#' * 28
 print('{}\n{}\n{}'.format(border, docstring, border))
 
 
-"""Use the inspect module again to get the docstring for any function being passed to the build_tooltip() function."""
+# Use the inspect module again to get the docstring for any function being passed to the build_tooltip() function.
 def build_tooltip(function):
   """Create a tooltip for any function that shows the 
   function's docstring.
