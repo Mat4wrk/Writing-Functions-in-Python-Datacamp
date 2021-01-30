a79553aa82044d233839f71406699295dfa74bb1
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 16:09:36 +0700

    Update Using decorator syntax.py

--- 3.Decorators/Using decorator syntax.py
+++ 3.Decorators/Using decorator syntax.py
@@ -1,4 +1,4 @@
-"""Decorate my_function() with the print_args() decorator by redefining my_function()."""
+# Decorate my_function() with the print_args() decorator by redefining my_function().
 def my_function(a, b, c):
   print(a + b + c)
 
@@ -8,7 +8,7 @@ my_function = print_args(my_function)
 my_function(1, 2, 3)
 
 
-"""Decorate my_function() with the print_args() decorator using decorator syntax."""
+# Decorate my_function() with the print_args() decorator using decorator syntax.
 # Decorate my_function() with the print_args() decorator
 @print_args
 def my_function(a, b, c):
