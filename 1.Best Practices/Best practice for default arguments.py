76690d32d176aa462e58fdce8bd21e92766776a7
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 16:01:12 +0700

    Update Best practice for default arguments.py

--- 1.Best Practices/Best practice for default arguments.py
+++ 1.Best Practices/Best practice for default arguments.py
@@ -1,17 +1,17 @@
 # Use an immutable variable for the default argument 
 def better_add_column(values, df=None):
-  """Add a column of `values` to a DataFrame `df`.
-  The column will be named "col_<n>" where "n" is
-  the numerical index of the column.
+#   Add a column of `values` to a DataFrame `df`.
+#   The column will be named "col_<n>" where "n" is
+#   the numerical index of the column.
 
-  Args:
-    values (iterable): The values of the new column
-    df (DataFrame, optional): The DataFrame to update.
-      If no DataFrame is passed, one is created by default.
+#   Args:
+#     values (iterable): The values of the new column
+#     df (DataFrame, optional): The DataFrame to update.
+#       If no DataFrame is passed, one is created by default.
 
-  Returns:
-    DataFrame
-  """
+#   Returns:
+#     DataFrame
+
   # Update the function to create a default DataFrame
   if df is None:
     df = pandas.DataFrame()
