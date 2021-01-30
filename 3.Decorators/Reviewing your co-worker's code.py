69c31c24522394a67da7ea9521f16dc2ce3e3cd1
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 16:09:00 +0700

    Update Reviewing your co-worker's code.py

--- 3.Decorators/Reviewing your co-worker's code.py
+++ 3.Decorators/Reviewing your co-worker's code.py
@@ -1,4 +1,4 @@
-"""Call has_docstring() on your co-worker's load_and_plot_data() function."""
+# Call has_docstring() on your co-worker's load_and_plot_data() function.
 # Call has_docstring() on the load_and_plot_data() function
 ok = has_docstring(load_and_plot_data)
 
@@ -8,7 +8,7 @@ else:
   print("load_and_plot_data() looks ok")
   
   
-"""Check if the function as_2D() has a docstring."""
+# Check if the function as_2D() has a docstring.
 # Call has_docstring() on the as_2D() function
 ok = has_docstring(as_2D)
 
@@ -18,7 +18,7 @@ else:
   print("as_2D() looks ok")
   
 
-"""Check if the function log_product() has a docstring."""
+# Check if the function log_product() has a docstring.
 # Call has_docstring() on the log_product() function
 ok = has_docstring(log_product)
 
