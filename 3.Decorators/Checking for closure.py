1830f2bea51a55e86e6c54d1d576883e2f1f5bdf
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 16:06:38 +0700

    Update Checking for closure.py

--- 3.Decorators/Checking for closure.py
+++ 3.Decorators/Checking for closure.py
@@ -1,4 +1,4 @@
-"""Use an attribute of the my_func() function to show that it has a closure that is not None."""
+# Use an attribute of the my_func() function to show that it has a closure that is not None.
 def return_a_func(arg1, arg2):
   def new_func():
     print('arg1 was {}'.format(arg1))
@@ -11,7 +11,7 @@ my_func = return_a_func(2, 17)
 print(my_func.__closure__ is not None)
 
 
-"""Show that there are two variables in the closure."""
+# Show that there are two variables in the closure.
 def return_a_func(arg1, arg2):
   def new_func():
     print('arg1 was {}'.format(arg1))
@@ -26,7 +26,7 @@ print(my_func.__closure__ is not None)
 print(len(my_func.__closure__) == 2)
 
 
-"""Get the values of the variables in the closure so you can show that they are equal to [2, 17], the arguments passed to return_a_func()."""
+# Get the values of the variables in the closure so you can show that they are equal to [2, 17], the arguments passed to return_a_func().
 def return_a_func(arg1, arg2):
   def new_func():
     print('arg1 was {}'.format(arg1))
