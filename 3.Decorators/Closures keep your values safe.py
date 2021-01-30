2795e3468cb77bd0ca0802c58c8361c07b87c439
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-30 16:07:31 +0700

    Update Closures keep your values safe.py

--- 3.Decorators/Closures keep your values safe.py
+++ 3.Decorators/Closures keep your values safe.py
@@ -1,4 +1,4 @@
-"""how that you still get the original message even if you redefine my_special_function() to only print "hello"."""
+# how that you still get the original message even if you redefine my_special_function() to only print "hello".
 def my_special_function():
   print('You are running my_special_function()')
   
@@ -16,7 +16,7 @@ def my_special_function():
 new_func()
 
 
-"""Show that even if you delete my_special_function(), you can still call new_func() without any problems."""
+# Show that even if you delete my_special_function(), you can still call new_func() without any problems.
 def my_special_function():
   print('You are running my_special_function()')
   
@@ -33,7 +33,7 @@ del(my_special_function)
 new_func()
 
 
-"""Show that you still get the original message even if you overwrite my_special_function() with the new function."""
+# Show that you still get the original message even if you overwrite my_special_function() with the new function.
 def my_special_function():
   print('You are running my_special_function()')
   
